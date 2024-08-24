56c2932c92d4f1630b6ed2b21bbe3bcd3e69a83c
LucasSdu <lucas.sdunnek@gmail.com>
2024-08-24 13:53:53 +0200

    mobil function

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -1,11 +1,18 @@
+import React from 'react';
 import Scene from './Scence';
 import Text from './Text';
+import useWindow from './useWindows';
 
 export default function Home() {
+  const { dimension } = useWindow();
+  
+  // Check if the user is on a mobile device
+  const isMobile = dimension.width <= 768;
+
   return (
     <main className="flex w-full h-screen items-center justify-center bg-white100">
       <Text />
-      <Scene />
+      {!isMobile && <Scene />} {/* Nur anzeigen, wenn es nicht mobil ist */}
     </main>
   );
 }
