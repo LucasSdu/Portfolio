28f05bb9a6e6fc44887088098063c6910dd70525
LucasSdu <147916073+LucasSdu@users.noreply.github.com>
2024-08-24 10:00:39 +0200

    name

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lucas Portfolio',
+  title: 'Lucas Sdunnek',
   description: 'Hello, this is my Portfolio. My name is Lucas Sdunnek',
 }
 
