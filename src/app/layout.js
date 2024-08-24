02d7af92a70f64eb708c6c067e7f2209f2a77400
LucasSdu <lucas.sdunnek@gmail.com>
2024-08-24 10:01:29 +0200

    change name

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lucas Sdunnek Portfolio',
+  title: 'Lucas Sdunnek',
   description: 'Willkommen bei meinen Portfolio. Mein Name ist Lucas Sdunnek',
 }
 
