b03299e44c677c1661e44b1e6659078c34186a19
LucasSdu <lucas.sdunnek@gmail.com>
2024-09-29 11:46:55 +0200

    google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Script from 'next/script'  // Importiere das Script-Modul von Next.js
+import Script from 'next/script' 
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,14 +9,12 @@ export const metadata = {
   description: 'Willkommen bei meinem Portfolio. Mein Name ist Lucas Sdunnek',
 }
 
-// Deine Google Analytics Tracking-ID (ersetze 'G-XXXXXXXXXX' durch deine ID)
 const GA_TRACKING_ID = 'G-GX97SM11DB';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Google Analytics Script */}
         <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
