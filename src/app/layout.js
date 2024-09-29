b9f78296081bee4aa1afc16632331820d6be9d1a
LucasSdu <lucas.sdunnek@gmail.com>
2024-09-29 11:44:38 +0200

    google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,42 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Script from 'next/script'  // Importiere das Script-Modul von Next.js
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Lucas Sdunnek',
-  description: 'Willkommen bei meinen Portfolio. Mein Name ist Lucas Sdunnek',
+  description: 'Willkommen bei meinem Portfolio. Mein Name ist Lucas Sdunnek',
 }
+
+// Deine Google Analytics Tracking-ID (ersetze 'G-XXXXXXXXXX' durch deine ID)
+const GA_TRACKING_ID = 'G-GX97SM11DB';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         {children}
-        </body>
+      </body>
     </html>
   )
 }
-
-
-
--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ import Projects from "../components/Projects";
 import AboutMe from "../components/Aboutme";
 import Text from "../components/Text";
 
-
 export default function Home() {
   const paragraph =
     "Hey, ich bin Lucas Sdunnek, studiere study Computervisualistik und Design in Lippstadt und habe eine Leidenschaft passion für gutes Design, Fotografie photography und Frontend-Entwicklung develop . Ich liebe heart es Interfaces zu designen mit einer wunderschönen und starken strong User Experience.";
@@ -31,25 +30,22 @@ export default function Home() {
       <main className={styles.main}>
         <div>
           <AboutMe />
-<div className={styles.topic}>
-<div className={styles.text}>Über mich</div>
+          <div className={styles.topic}>
+            <div className={styles.text}>Über mich</div>
 
-  <hr style={{ border: '1px solid #BCF906', width: '30%' }} />
-</div>
-          
+            <hr style={{ border: "1px solid #BCF906", width: "30%" }} />
+          </div>
 
-
-        <div className={styles.aboutme}>
-        <Text paragraph={paragraph} />
-        </div>
-         
+          <div className={styles.aboutme}>
+            <Text paragraph={paragraph} />
+          </div>
         </div>
 
         <div className={styles.topic}>
-<div className={styles.text}>Projekte</div>
+          <div className={styles.text}>Projekte</div>
 
-  <hr style={{ border: '1px solid #BCF906', width: '30%' }} />
-</div>
+          <hr style={{ border: "1px solid #BCF906", width: "30%" }} />
+        </div>
         <div className={styles.containerProjects}>
           <Projects />
         </div>
